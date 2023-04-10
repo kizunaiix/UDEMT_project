e07ec5e3821667cd44ab8032979c7410403d57e2
kizunaiix <yaodong.sun@outlook.com>
2023-04-10 14:52:46 +0200

    给off2ply加了注释

---
+++ 2-15/python/smalltools.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def pc_normalize(pc):
+    centroid = np.mean(pc, axis=0)
+    pc = pc - centroid
+    m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
+    pc = pc / m
+    return pc
