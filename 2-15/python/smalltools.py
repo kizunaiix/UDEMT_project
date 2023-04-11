f968b85d78b27cff5c674b624c1718dc981a1218
kizunaiix <yaodong.sun@outlook.com>
2023-04-11 21:06:19 +0200

    未完成：stl转ply的demo

--- 2-15/python/smalltools.py
+++ 2-15/python/smalltools.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 def pc_normalize(pc):
+    '''
+    将点云的点归一化并将中心放置在原点
+    @param pc: (n,3)float
+    '''
     centroid = np.mean(pc, axis=0)
     pc = pc - centroid
     m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
